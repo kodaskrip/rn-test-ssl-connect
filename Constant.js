1f5967da4d9e6cb58ba23e96c99186219baf9876
Fajri Rahmat <fajrirahmat@Fajris-MacBook-Pro.local>
2019-06-21 14:42:37 +0700

    fix https android issue

--- Constant.js
+++ Constant.js
@@ -2,7 +2,6 @@ export default {
   baseUrl: 'https://api-dev-1.daya-dms.id',
   commonHeaders: {
     'Content-Type': 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiJ9.5BG9SEVOGo_xRhtT8IkyoSy60kPg8HM9Vpvb0TdNew4'
+    Authorization: 'Bearer **********************************'
   }
 };
